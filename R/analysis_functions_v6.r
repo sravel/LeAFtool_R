f9ccae0bd5f33da091168c0fe16b3b5830358054
sravel <sebastien.ravel@cirad.fr>
2019-05-29 11:33:30 +0200

    Add V6

---
+++ R/analysis_functions_v6.r
@@ -0,0 +1,214 @@
+options(width=160)
+
+## load packages
+library(EBImage)
+library(MASS)
+## library(e1071) only for svm (not implemented)
+
+## returns the range of indexes of objet in vector x
+## (called by bounding.rectangle)
+range.na <- function(x, object) {
+    w <- which(x==object)
+    if (length(w)==0) return(c(NA,NA))
+    return(range(w))
+}
+
+bounding.rectangle <- function(mask, object) {
+    m <- imageData(mask)
+    range.x <- range(apply(m,1,range.na,object),na.rm=TRUE)
+    range.y <- range(apply(m,2,range.na,object),na.rm=TRUE)
+    list(x=range.x[1]:range.x[2],y=range.y[1]:range.y[2])
+}
+
+extract.leaf <- function(i,mask,image.black.background) {
+    b <- bounding.rectangle(mask,i)
+    leaf <- image.black.background[b$y,b$x,]
+    mask.leaf <- mask[b$y,b$x]
+    leaf[mask.leaf!=i] <- 0
+    list(b=b,leaf=leaf)
+}
+
+predict2 <- function(image,train) { ## returns predicted values according to train
+    df <- data.frame(red=as.numeric(imageData(image)[,,1]), green=as.numeric(imageData(image)[,,2]), blue=as.numeric(imageData(image)[,,3]))
+    if (train$colormodel=="hsv") df <- rgb2hsv2(df)
+    if (!is.null(train$transform)) df[1:3] <- lapply(df,train$transform)
+    if (train$method=="lda" || train$method=="qda")
+        return(predict(train$lda1,df)$class)
+    else if (train$method=="svm")
+        return(predict(train$svm,df))
+}
+
+## analyse a single leaf
+analyse.leaf <- function(x,train,lesion.border,lesion.area.min,lesion.area.max,lesion.eccentricity.max,blur.diameter) {
+    ## remplace background with limb
+    f <- x$leaf
+    limb <- train$classes$subclass[train$classes$class=="limb"]
+    lesion <- train$classes$subclass[train$classes$class=="lesion"]
+    df6 <- data.frame(red=as.numeric(imageData(f)[,,1]), green=as.numeric(imageData(f)[,,2]), blue=as.numeric(imageData(f)[,,3]))
+    black.background <- df6$red+df6$green+df6$blue==0
+    prediction <- predict2(f,train)
+    df.limb <- df6[prediction %in% limb & !black.background,]
+    mean.limb <- apply(df.limb[1:3],2,mean)
+    df6[black.background,1] <- mean.limb[1]
+    df6[black.background,2] <- mean.limb[2]
+    df6[black.background,3] <- mean.limb[3]
+    imageData(f)[,,1] <- df6$red
+    imageData(f)[,,2] <- df6$green
+    imageData(f)[,,3] <- df6$blue
+
+    ## analyse
+    if (blur.diameter>1) { ## blurring
+        ##x11() ; display(f,method="raster")
+        flo <- makeBrush(blur.diameter, shape='disc', step=FALSE)^2
+        flo <- flo/sum(flo)
+        f <- filter2(f, flo)
+        f[f<0] <- 0
+        ##x11() ; display(f,method="raster")
+    }
+    prediction <- predict2(f, train)
+    patch <- as.numeric(prediction %in% lesion)
+    mask <- channel(f, "gray")
+    patch.mat <- matrix(patch, nrow=nrow(imageData(mask)))
+    imageData(mask) <- patch.mat
+
+    ## dilate
+    brush <- makeBrush(lesion.border, shape='disc')
+    mask <- dilate(mask, brush)
+
+    ## fill empty areas
+    mask <- fillHull(mask)
+
+    ## erode
+    mask <- erode(mask, brush)
+
+    ## segmentation
+    mask[mask<0] <- 0
+    mask <- bwlabel(mask)
+    features <- computeFeatures.shape(mask)
+    moments <- computeFeatures.moment(mask)
+
+    ## remove small objects
+    w.small <- w.large <- w.eccentric <- NULL
+    w.small <- which(features[,"s.area"]<lesion.area.min)
+    w.large <- which(features[,"s.area"]>lesion.area.max)
+    w.eccentric <- which(moments[,"m.eccentricity"]>lesion.eccentricity.max)
+    mask[mask %in% c(w.small,w.large,w.eccentric)] <- 0
+
+    features <- computeFeatures.shape(mask)
+    list(features=features, mask=mask)
+}
+
+#' Analyse an image or a set of images
+#'
+#' @param path.sample The path of the directory containing the sampled images used for training. After the training step, this directory contains the parameters of the training set.
+#' @param path.result The path of the directory where to store the result files (created by the function if it does not exist).
+#' @param path.image The path of the directory containing the images to analyse.
+#' @param file.image A character vector containg the fils names of the images to analyse in path.image (NA to analyse all the images in path.image).
+#' @param leaf.area.min The minimum area of a leaf (in pixels).
+#' @param leaf.border The diameter of the brush (in pixels) used to erode the leaf border.
+#' @param lesion.border The diameter of the brush (in pixels) used to erode the lesion border.
+#' @param lesion.area.min The minimum area of a lesion (in pixels).
+#' @param lesion.area.max The maximum area of a lesion (in pixels).
+#' @param lesion.eccentricity.max The maximum eccentricity of a lesion.
+#' @param lesion.color Lesion color in the output image.
+#' @param blur.diameter The diameter of the brush (in pixels) used to blur the image (0 for no blur).
+analyse.image <- function(path.sample,path.result,path.image,file.image=NA,leaf.area.min,leaf.border,lesion.border,lesion.area.min,lesion.area.max,lesion.eccentricity.max,lesion.color,blur.diameter) {
+    if (is.na(file.image)) file.image <- list.files(path.image)
+    for (image in file.image)
+        analyse.image.unique(path.sample,path.result,path.image,image,leaf.area.min,leaf.border,lesion.border,lesion.area.min,lesion.area.max,lesion.eccentricity.max,lesion.color,blur.diameter)
+}
+
+analyse.image.unique <- function(path.sample,path.result,path.image,file.image,leaf.area.min,leaf.border,lesion.border,lesion.area.min,lesion.area.max,lesion.eccentricity.max,lesion.color,blur.diameter) {
+    ## load trainig results
+    filename <- tail(strsplit(path.sample,'/')[[1]],1)
+    file.train <- paste(path.sample,'/',filename,".RData",sep='')
+    load(file.train)
+
+    background <- train$classes$subclass[train$classes$class=="background"]
+    limb <- train$classes$subclass[train$classes$class=="limb"]
+    lesion <- train$classes$subclass[train$classes$class=="lesion"]
+
+    ## load source image
+    source.image <- paste(path.image,'/',file.image,sep='')
+    image <- readImage(source.image)
+
+    prediction <- predict2(image,train)
+
+    ## create patch and leaf identifiers
+    patch <- as.numeric(prediction %in% lesion)
+    leaf <- as.numeric(!(prediction %in% background))
+
+    ## create leaf mask
+    mask <- channel(image, "gray")
+    leaf.mat <- matrix(leaf, nrow=nrow(imageData(mask)))
+    imageData(mask) <- leaf.mat
+
+    ## fill empty areas
+    mask <- fillHull(mask)
+
+    ## delete border by erosion
+    ## remark: tests and computations are made with eroded objects
+    brush <- makeBrush(leaf.border, shape='disc')
+    ## uncomment the following line to remove abnormal stripes from the scan
+    ## brush2 <- makeBrush(leaf.border*2+1, shape='disc') ; mask <- dilate(mask,brush2) ; mask <- erode(mask, brush2)
+    mask <- erode(mask, brush)
+
+    ## segmentation
+    mask <- bwlabel(mask)
+    features <- data.frame(computeFeatures.shape(mask))
+
+    ## remove objects smaller than the minimum leaf area
+    w <- which(features[,"s.area"]<leaf.area.min)
+    if (length(w) > 0) {
+        mask[mask %in% w] <- 0
+        features <- features[-w,]
+    }
+
+    ## delete background
+    image.black.background <- image
+    image.black.background[mask==0] <- 0
+
+    ## separate the leaaves
+    li <- lapply(as.numeric(row.names(features)),extract.leaf,mask,image.black.background)
+
+    ## analyse the leaves
+    analyse.li <- lapply(li,analyse.leaf,train,lesion.border,lesion.area.min,lesion.area.max,lesion.eccentricity.max,blur.diameter)
+
+    ## create output file names
+    filename <- strsplit(file.image,'\\.')[[1]][1]
+    pdfname <- paste(filename,".pdf",sep='')
+    txtname1 <- paste(filename,"_1.txt",sep='')
+    txtname2 <- paste(filename,"_2.txt",sep='')
+    if (!file.exists(path.result)) dir.create(path.result)
+    pdffile <- paste(path.result,'/',pdfname,sep='')
+    txtfile1 <- paste(path.result,'/',txtname1,sep='')
+    txtfile2 <- paste(path.result,'/',txtname2,sep='')
+
+    ## write results
+    pdf(pdffile)
+    display(image, method="raster", all=TRUE)
+
+    result <- NULL
+    for (i in 1:length(li)) {
+        result <- rbind(result,data.frame(file=filename,leaf=i,leaf.area=features[i,"s.area"],lesion.area=if (is.null(analyse.li[[i]]$features)) 0 else analyse.li[[i]]$features[,"s.area"]))
+        tmpimage <- image[li[[i]]$b$y,li[[i]]$b$x,]
+        tmpimage[analyse.li[[i]]$mask>0] <- lesion.color
+        image[li[[i]]$b$y,li[[i]]$b$x,] <- tmpimage
+    }
+    row.names(result) <- NULL
+
+    display(image, method="raster", all=TRUE)
+    dev.off()
+
+    write.table(result,file=txtfile1,quote=FALSE,row.names=FALSE,sep='\t')
+
+    ag.count <- aggregate(result$lesion.area,result[c("file","leaf", "leaf.area")],length)
+    names(ag.count)[4] <- "lesion.count"
+    ag.area <- aggregate(result$lesion.area,result[c("file","leaf","leaf.area")],sum)
+    names(ag.area)[4] <- "lesion.area"
+    ag <- merge(ag.count,ag.area)
+    ag$percent.lesion <- ag$lesion.area/ag$leaf.area*100
+    ag$lesion.count[ag$lesion.area==0] <- 0
+
+    write.table(ag[order(ag$leaf),],file=txtfile2,quote=FALSE,row.names=FALSE,sep='\t')
+}
---
+++ R/training_functions_V6.r
@@ -0,0 +1,154 @@
+options(width=160)
+
+## load packages
+library(EBImage)
+library(lattice)
+library(MASS)
+## library(e1071) only for svm (not implemented)
+
+table2 <- function(x,y) {
+    ta <- table(x,y,dnn=c("group","predict"))
+    print(ta)
+    paste("\nError rate: ",round((1-sum(diag(ta))/sum(ta))*100,2),'%\n\n')
+}
+
+lastname <- function(fullname) { ## extracts directory names from full file name
+    li <- strsplit(fullname,'/')
+    tail(li[[1]],2)[1]
+}
+
+load.subgroup <- function(sg) { ## load images from directory sg
+    files.subgroup <- list.files(sg,full.name=TRUE, pattern = "\\.jpg$|\\.jpeg$|\\.PNG$|\\.tif$", include.dirs = FALSE, ignore.case = TRUE)
+    li <- lapply(files.subgroup,function(file) {
+        im <- readImage(file)
+        data.frame(group=lastname(file),red=as.numeric(imageData(im)[,,1]), green=as.numeric(imageData(im)[,,2]), blue=as.numeric(imageData(im)[,,3]))
+    })
+    do.call(rbind, li)
+}
+
+load.group <- function(g) { ## load the images of group g (which can contain subdirectories)
+    ## search for sud-directories
+    dirs <- list.dirs(g,recursive=FALSE)
+    if (length(dirs)==0) { ## no subdirectories (only files)
+        return(load.subgroup(g))
+    }
+    ## load images from subdirectories
+    li <- lapply(dirs,load.subgroup)
+    do.call(rbind,li)
+}
+
+load.class <- function(class,path.sample) { ## load all the training images
+    path.class <- paste(path.sample,class,sep='/')
+    if (!all(file.exists(path.class))) stop("Directory not found.")
+    li <- lapply(path.class,load.group)
+    do.call(rbind,li)
+}
+
+rgb2hsv2 <- function(rgb) { ## convert a data frame from rgb to hsv
+    w <- match(c("red","green","blue"),names(rgb))
+    hsv <- t(rgb2hsv(t(rgb[w])))
+    rgb[w] <- hsv
+    names(rgb)[w] <- colnames(hsv)
+    rgb
+}
+
+
+#' Compute and saves on disk the parameters of the training set
+#'
+#' @param path.sample The path of the directory containing sampled images for training. This directory must contain at least 3 directories (for background, limb, and lesion images).
+#' @param background The name of the directory in path.sample containing sampled images of the background. This directory can contain either image files or subdirectories containing different groups of image files.
+#' @param limb The name of the directory in path.sample containing sampled images of the limb. This directory can contain either image files or subdirectories containing different groups of image files.
+#' @param lesion The name of the directory in path.sample containing sampled images of lesions. This directory can contain either image files or subdirectories containing different groups of image files.
+#' @param method Method of discrimainant analysis: "lda" (default) or "qda"
+#' @param transform Function for data transformation before analysis (e.g. sqrt)
+#' @param colormodel Model of color for the analysis: "rgb" (default) or "hsv"
+#'
+#' @examples
+#' path.sample <- "../Exemple_Dominique/CLFD/Samples/Sup"
+#'
+#' training(.path.sample,"background","limb","lesion")
+#' training(path.sample,"background","limb","lesion",transform=function(x) log1p(x),colormodel="rgb",method="svm")
+#' training(path.sample,"background","limb","lesion",colormodel="hsv",method="lda")
+#' training(path.sample,"background","limb","lesion",transform=function(x) (sin(pi*(x-0.5))+1)/2,method="qda")
+#' training(path.sample,"background","limb","lesion",transform=function(x) asin(2*x-1)/pi+0.5)
+#' training(path.sample,"background","limb","lesion",transform=log1p)
+(training) <- function(path.sample,background,limb,lesion,method="lda",transform=NULL,colormodel="rgb") {
+    version <- "6.0"
+    stopifnot(method %in% c("lda","qda","svm"))
+    stopifnot(colormodel %in% c("rgb","hsv"))
+    ## search for subdirectories in path.sample
+    dirs <- list.dirs(path.sample,recursive=FALSE,full.names=FALSE)
+
+    ## check the existence of the subdirectories passed in argument
+    class <- c(background,limb,lesion)
+    li <- lapply(class,load.class,path.sample)
+    groups.li <- lapply(li,function(x) unique(x$group))
+    classes <- rbind(data.frame(class="background",subclass=groups.li[[1]]),data.frame(class="limb",subclass=groups.li[[2]]),data.frame(class="lesion",subclass=groups.li[[3]]))
+    groups <- unlist(groups.li)
+    if (any(duplicated(groups))) stop("Error: duplicated group names.")
+    df2 <- do.call(rbind, li)
+    if (colormodel=="hsv") df2 <- rgb2hsv2(df2)
+    if (!is.null(transform)) df2[2:4] <- lapply(df2[2:4],transform)
+
+    ## split df2 into train and test for cross-validation
+    type <- rep("train",nrow(df2))
+    type[sample(1:length(type),length(type)/2)] <- "test"
+    df.train <- df2[type=="train",]
+    df.test <- df2[type=="test",]
+
+    ## discriminant analysis
+    ## compute lda1 for graphic output (even if method is not "lda")
+    lda1 <- lda(df2[2:4], df2$group, prior=rep(1,length(groups))/length(groups))
+    df4 <- cbind(df2, as.data.frame(as.matrix(df2[2:4])%*%lda1$scaling))
+
+    if (method=="lda") {
+        lda2 <- lda(df.train[2:4], df.train$group, prior=rep(1,length(groups))/length(groups))
+    }
+    else if (method=="qda"){
+        lda1 <- qda(df2[2:4], df2$group, prior=rep(1,length(groups))/length(groups))
+        lda2 <- qda(df.train[2:4], df.train$group, prior=rep(1,length(groups))/length(groups))
+    }
+    else if (method=="svm") { ## only if library e1071 is installed
+        svm1 <- svm(group~red+green+blue, data=df2, kernel ="radial", gamma =10, cost =1.8)
+        svm2 <- svm(group~red+green+blue, data=df.train, kernel ="radial", gamma =10, cost =1.8)
+   }
+
+    ## common name for the 3 output files, identique to the directory name
+    filename <- tail(strsplit(path.sample,'/')[[1]],1)
+
+    ## save results in text file
+    file.txt <- paste(path.sample,paste0(filename,".txt"),sep='/') ## text output file
+    sink(file.txt)
+    cat("Version",version,'\n')
+    if (!is.null(transform)) {
+        cat("transform:\n")
+        print(transform)
+    }
+    cat("colormodel:",colormodel,'\n')
+    cat("method:",method,'\n')
+    print(table(df2$group))
+    cat('\n')
+    if (method=="lda" || method=="qda") {
+        df.test$predict <- predict(lda2, df.test[2:4])$class
+        print(lda1$scaling)
+        train <- list(version=version,lda1=lda1,classes=classes,transform=transform,colormodel=colormodel,method=method)
+    }
+    else if (method=="svm") { ## only if library e1071 is installed
+        df.test$predict <- predict(svm2, df.test)
+        print(svm1)
+        train <- list(version=version,svm11=svm1,classes=classes,transform=transform,colormodel=colormodel,method=method)
+    }
+    cat('\n')
+    cat(table2(df.test$group, df.test$predict))
+    sink()
+
+    ## graph of the groups in axes 1 and 2 of discriminant analysis
+    file.png <- paste(path.sample,paste0(filename,".png"),sep='/') ## fichier de sortie png
+    png(file.png,width=480*6,height=480*6,res=72*6)
+    print(xyplot(LD2~LD1, group=group, cex=0.8, alpha=1, pch=1, asp=1, auto.key=TRUE, data=df4))
+    dev.off()
+
+    ## save results
+    file.train <- paste(path.sample,paste0(filename,".RData"),sep='/')
+    save(train,file=file.train)
+}
